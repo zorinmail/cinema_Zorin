251bf5452f7d268ab951f0fa9c2ece629339097b
zorinmail <zorinmail@yandex.ru>
2019-10-20 14:20:46 +0500

    4.4.1 Формирование таблицы фильмов из массива

--- js/films.js
+++ js/films.js
@@ -1,36 +1,151 @@
-const genre_1 = 'фантастика'
-const genre_2 = 'боевик'
-const genre_3 = 'приключения'
-const genre_4 = 'фэнтези'
-const genre_5 = 'драма'
-const genre_6 = 'комедия'
-const genre_7 = 'мультфильм'
+// const genre_1 = 'фантастика'
+// const genre_2 = 'боевик'
+// const genre_3 = 'приключения'
+// const genre_4 = 'фэнтези'
+// const genre_5 = 'драма'
+// const genre_6 = 'комедия'
+// const genre_7 = 'мультфильм'
 
-let start_film_1 = '10:00'
-let start_film_2 = '12:00'
-let start_film_3 = '14:00'
-let start_film_4 = '16:00'
+// let start_film_1 = '10:00'
+// let start_film_2 = '12:00'
+// let start_film_3 = '14:00'
+// let start_film_4 = '16:00'
 
-let name_film_1 = 'Человек-паук 123'
-let name_film_2 = 'Собачья жизнь 2'
-let name_film_3 = 'История игрушек 4'
-let name_film_4 = 'Люди в черном: Интернэшнл'
+// let name_film_1 = 'Человек-паук 123'
+// let name_film_2 = 'Собачья жизнь 2'
+// let name_film_3 = 'История игрушек 4'
+// let name_film_4 = 'Люди в черном: Интернэшнл'
 
-//console.log('фильм 1 ', name_film_1)
+// //console.log('фильм 1 ', name_film_1)
 
-let genre_film_1 = genre_1 + ', ' + genre_2 + ', ' + genre_3
-let genre_film_2 = genre_4 + ', ' + genre_5 + ', ' + genre_6
-let genre_film_3 = genre_7 + ', ' + genre_4 + ', ' + genre_6
-let genre_film_4 = genre_1 + ', ' + genre_2 + ', ' + genre_6
+// let genre_film_1 = genre_1 + ', ' + genre_2 + ', ' + genre_3
+// let genre_film_2 = genre_4 + ', ' + genre_5 + ', ' + genre_6
+// let genre_film_3 = genre_7 + ', ' + genre_4 + ', ' + genre_6
+// let genre_film_4 = genre_1 + ', ' + genre_2 + ', ' + genre_6
 
 //console.log('элемент 1 ', document.getElementById('name_film_1'))
 
-let element_start_film_1 = document.getElementById('start_film_1')
-let element_name_film_1 = document.getElementById('name_film_1')
-let element_genre_film_1 = document.getElementById('genre_film_1')
+films = [
+    film1 = {
+        name: 'Человек-Паук',
+        start: '12:00',
+        genre: [0, 1],
+        hire: true,
+        new: false,
+        description: 'Lorem ipsum'
+    },
+    film2 = {
+        name: 'Собачья жизнь 2',
+        start: '14:00',
+        genre: [2, 3, 4],
+        hire: true,
+        new: false,
+        description: 'Lorem ipsum'
+    },
+    film3 = {
+        name: 'История игрушек 4',
+        start: '16:00',
+        genre: [2, 4, 5],
+        hire: true,
+        new: false,
+        description: 'Lorem ipsum'
+    },
+    film4 = {
+        name: 'Люди в черном',
+        start: '18:00',
+        genre: [0, 1, 4],
+        hire: false,
+        new: true,
+        description: 'Lorem ipsum'
+    },
+    film5 = {
+        name: 'История игрушек 6',
+        start: '16:00',
+        genre: [2, 4, 5],
+        hire: false,
+        new: true,
+        description: 'Lorem ipsum'
+    },
+    film6 = {
+        name: 'Люди в черном 6',
+        start: '18:00',
+        genre: [0, 1, 4],
+        hire: false,
+        new: true,
+        description: 'Lorem ipsum'
+    }
+]
 
-//console.log('element_start_film_1 ', element_start_film_1)
+//словарь жанров
+const genres = [
+    'Фантастика', //0
+    'Боевик',     //1
+    'Фэнтези',    //2
+    'Драма',      //3
+    'Комедия',    //4
+    'Мультфильм'  //5
+]
 
-element_start_film_1.innerHTML = start_film_1
-element_name_film_1.innerHTML = name_film_1
-element_genre_film_1.innerHTML = genre_film_1
+
+
+let hireFilms = []
+let newFilm = []
+
+for (i = 0; i < films.length; i++){
+    //console.log(films[i])
+    if (films[i].hire == true) {
+        // console.log(films[i])
+        hireFilms.push(films[i])
+    }
+    if (films[i].hire == false && films[i].new == true) {
+        // console.log(films[i])
+        newFilm.push(films[i])
+    }
+}
+console.log(hireFilms)
+console.log(newFilm)
+
+const film = {
+    getName: function () {
+        console.log(this.name)
+    },
+    getStart: function () {
+        console.log(this.start)
+    },
+    getGenre: function () {
+        const genresFilm = this.genre
+        let arrGenre = []
+        for (i = 0; i < genres.length; i++) {
+            //console.log(genresFilm[i])
+            arrGenre.push(genres[genresFilm[i]])
+        }
+        let strGenre = arrGenre.join(',')
+        console.log('жанры', strGenre)
+    }
+}
+
+for (let i = 0; i< hireFilms.length; i++) {
+    дуе ашдь
+}
+
+
+for (let i = 0; i < hireFilms.length; i++) {
+    film.getName.bind(hireFilms[i])()
+    film.getStart.bind(hireFilms[i])()
+    film.getGenre.bind(hireFilms[i])()
+
+}
+
+
+
+
+
+// let element_start_film_1 = document.getElementById('start_film_1')
+// let element_name_film_1 = document.getElementById('name_film_1')
+// let element_genre_film_1 = document.getElementById('genre_film_1')
+
+// //console.log('element_start_film_1 ', element_start_film_1)
+
+// element_start_film_1.innerHTML = start_film_1
+// element_name_film_1.innerHTML = name_film_1
+// element_genre_film_1.innerHTML = genre_film_1
---
+++ js/films_4.4.js
@@ -0,0 +1,96 @@
+let films = [
+    ['10:00', 'Человек-паук', [0, 1, 6], 'https://www.kinopoisk.ru/film/1008445/'],
+    ['12:00', 'Собачья жизнь 2', [2, 3, 4], 'https://www.kinopoisk.ru/film/sobachya-zhizn-2-2019-1122114/'],
+    ['14:00', 'История игрушек 4', [5, 2, 4], 'https://www.kinopoisk.ru/film/istoriya-igrushek-4-2019-846824/'],
+    ['16:00', 'Люди в черном: Интернэшнл', [0, 1, 4], 'https://www.kinopoisk.ru/film/lyudi-v-chernom-interneshnl-2019-693730/'],
+    ['18:00', 'Lego movie', [5, 4, 2], 'https://www.kinopoisk.ru/film/lyudi-v-chernom-interneshnl-2019-693730/']
+]
+//let films = []
+
+//словарь жанров
+const genres = [
+    'фантастика', //0
+    'боевик',     //1
+    'фэнтези',    //2
+    'драма',      //3
+    'комедия',    //4
+    'мультфильм', //5
+    'приключения' //6
+]
+
+
+
+function createTableFilms(films, genres) {
+
+    let table_body = document.getElementById('movie-list__body')
+
+    //сначала очищаем таблицу (наверное, потом может понадобиться при сортировке и т.п.)
+    while (table_body.firstChild) {
+        table_body.removeChild(table_body.firstChild);
+    }
+    //console.log(table_body)
+
+    let filmGenre = []
+    let filmsGenres = []
+
+    //переводим жанры из номеров в слова
+    for (let i = 0; i < films.length; i++){
+        filmGenre = []
+        for (let j = 0; j < (films[i][2]).length; j++) {
+            filmGenre.push(genres[films[i][2][j]])
+        }
+        filmsGenres.push(filmGenre)
+    }
+
+    let i = 0;          //для цикла do...while
+    let k = 0;          //формирование id ячеек таблицы
+    let filmStart = ''; //начало фильма
+    let filmName = '';  //название фильма
+    let strGenre = '';  //жанры
+    let trClass = '';   //класс для цвета четных и нечетных строк
+    let linkFilm = '';  //ссылка ка кинопоиск
+
+    do {
+        if (films.length == 0){ //если массив с фильмами пуст
+            //добавить запись об отсутствии в прокате фильмов
+            table_body.insertAdjacentHTML('beforeend', '<tr class="movie-list__table-dark">\
+            <td colspan="4" class="movie-list__table-film-b">К сожалению, сейчас фильмов нет</td>\
+            </tr>');
+            break;
+        } else { //если массив с фильмами не пуст, по очереди добавляем фильмы в таблицу
+            strGenre = ''
+            k = i + 1;
+            filmStart = films[i][0];
+            filmName = films[i][1];
+            strGenre = filmsGenres[i].join(', ');
+            linkFilm = films[i][3]
+
+            //определение четности строки
+            if ((i+1)%2 != 0) {
+                trClass = 'movie-list__table-dark'
+            } else {
+                trClass = 'movie-list__table-light'
+            }
+
+            table_body.insertAdjacentHTML('beforeend', `<tr class="${trClass}">\
+                <td class="movie-list__table-time-b"  id="start_film_${k}">${filmStart}</td>\
+                <td class="movie-list__table-film-b"><a title="Кинопоиск" href="${linkFilm}"\
+                    target="_blank" id="name_film_${k}">${filmName}</a></td>\
+                <td class="movie-list__table-genre-b" id="genre_film_${k}">${strGenre}</td>\
+                <td class="movie-list__table-plus">\
+                    <svg \
+                    xmlns="http://www.w3.org/2000/svg"\
+                    xmlns:xlink="http://www.w3.org/1999/xlink"\
+                    width="33px" height="33px">\
+                    <path fill-rule="evenodd"  fill="rgb(255, 255, 255)"\
+                    d="M16.505,32.994 C7.393,32.994 0.005,25.607 0.005,16.494 C0.005,7.381 7.393,-0.006 16.505,-0.006 C25.618,-0.006 33.005,7.381 33.005,16.494 C33.005,25.607 25.618,32.994 16.505,32.994 ZM24.214,14.591 L18.356,14.631 L18.356,8.796 L13.958,8.777 L13.958,14.637 L8.083,14.637 L8.083,19.074 L13.958,19.074 L13.958,24.889 L18.336,24.889 L18.307,19.074 L24.231,19.074 L24.214,14.591 Z"/>\
+                    </svg>\
+                </td>\
+            </tr>`)
+        }
+        i++
+    } while (i < films.length)
+}
+
+
+createTableFilms(films, genres)
